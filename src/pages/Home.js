39bb5a17bb89b27b60cbf7b245f254496f226bcb
hrachgalstyan <hrach_002@mail.ru>
2020-04-15 21:46:16 +0400

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+import Services from '../components/Services'
+import FeaturedRooms from '../components/FeaturedRooms'
+export default function Home() {
+    return (
+        <>
+        <Hero>
+            <Banner title="Luxurious rooms" subtitle="Deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">Our rooms</Link>
+            </Banner>
+        </Hero>
+        <Services />
+        <FeaturedRooms />
+        </>
+    )
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+
+export default function Rooms() {
+    return (
+        <Hero hero="roomsHero">
+            <Banner title="Our rooms">
+                <Link to="/" className="btn-primary">
+                    Return Home
+                </Link>
+            </Banner>
+        </Hero>
+    )
+}
