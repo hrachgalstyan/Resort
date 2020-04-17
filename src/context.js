a135c562198c982ffb6f5dab05acffb407f82c2a
hrachgalstyan <hrach_002@mail.ru>
2020-04-17 15:59:17 +0400

    finish

--- src/context.js
+++ src/context.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react'
-import items from './data'
 import Client from './Contentful'
-
-Client.getEntries({
-    content_type: "beachResortRoom"
-}).then(response => console.log(response.items));
 
 const RoomContext = React.createContext();
 
@@ -25,23 +20,33 @@ class RoomProvider extends Component {
         pets: false
     }
     // getData
-
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: "beachResortRoom"
+            });
+            
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms, 
+                featuredRooms, 
+                sortedRooms: rooms, 
+                loading:false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
 
     componentDidMount(){
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
-        this.setState({
-            rooms, 
-            featuredRooms, 
-            sortedRooms: rooms, 
-            loading:false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        });
+        this.getData()
     }
 
     formatData(items){
@@ -62,7 +67,6 @@ class RoomProvider extends Component {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked:target.value;
         const name = event.target.name;
-        console.log(target,value,name);
         this.setState({
             [name]:value
         }, this.filterRooms);
